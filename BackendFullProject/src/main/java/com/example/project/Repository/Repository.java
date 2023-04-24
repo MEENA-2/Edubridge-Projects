9e8a466d56228847603f3457be3f5106b5deb5b9
Meena Priyadharshini S R <83598565+MEENA-2@users.noreply.github.com>
2023-04-24 15:06:41 +0530

    Delete BackendFullProject directory

--- BackendFullProject/src/main/java/com/example/project/Repository/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.project.Student.Student;
-
-public interface Repository extends JpaRepository<Student, Integer>{
-
-}
